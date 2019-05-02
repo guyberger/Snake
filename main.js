0c66e532e62881bed90e48e66aaea462cbe94c94
גיא ברגר <guyber1992@gmail.com>
2019-05-02 14:59:54 +0300

    improved functionality

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
+document.getElementById('restart').addEventListener("click", function()
+{
+    console.log('restarting');
+    var restart_btn = document.getElementById('restart');
+    restart_btn.style.display = 'none';
+    startGame();
+})
+
 //Global settings variables 
 var alive = true;
 var speed = 100;    //miliseconds
 var key_allowed = true;
+var s;  // Snake
 
 window.onkeyup = function(e) {
     if(!key_allowed) return;
@@ -30,8 +39,23 @@ window.onkeyup = function(e) {
 
 function endGame(){
     alive = false;
-    //free resources
+    key_allowed = true;
+
+    // remove the snake
+
+    var snake = document.getElementsByClassName('fa-facebook');
+    while(snake[0]){
+        snake[0].parentNode.removeChild(snake[0]);
+    }
+
+    var food = s.food;
+    var foodNode = document.getElementById('food-' + food[0] + '-' + food[1]);
+    foodNode.parentElement.removeChild(foodNode);
+
     console.log('dead');
+
+    var restart_btn = document.getElementById('restart');
+    restart_btn.style.display = 'block';
 }
 
 //Box in which the snake moves and drawn
@@ -43,10 +67,12 @@ class Box{
     }
     draw(){
         var board = document.getElementById('game-board');
-        var node = document.createElement('div');
-        node.className = 'box';
+        var node = document.createElement('i');
+        node.className = 'fab fa-facebook box';
+        node.setAttribute("aria-hidden", "true");
         node.id = this.name;
-        node.style.backgroundColor = 'black';
+        node.style.fontSize = '1.5em';
+        //node.style.backgroundColor = 'black';
         node.style.left = this.x + 'px';
         node.style.top = this.y + 'px';
         board.appendChild(node);
@@ -63,7 +89,7 @@ function drawBox(startx,starty){
     node.className = 'box';
     node.id = 'box-' + startx + '-' + starty;
     
-    // To the board's boxes - uncomment
+    // To see the board's boxes - uncomment
     /*
     var bgColor = node.style.backgroundColor;
     if(startx % 2 || starty % 2){
@@ -73,6 +99,7 @@ function drawBox(startx,starty){
         node.style.backgroundColor = bgColor;
     }
     */
+    node.style.backgroundColor = 'none';
     node.style.left = startx + 'px';
     node.style.top = starty + 'px';
     board.appendChild(node);
@@ -109,8 +136,12 @@ class Snake{
                 addx -= 25; break;
         }
         var addbox = new Box(curr.x + addx, curr.y + addy);
-        addbox.draw();
         this.boxes.unshift(addbox);
+        if(this.isDead()) {
+            this.boxes.shift();
+            endGame();
+        }
+        else addbox.draw();
     }
 
     remove(){
@@ -140,19 +171,13 @@ class Snake{
 
     // Move the snake
     move(){
-        if(this.isDead()){
-            endGame();
+        this.grow();
+        if(this.boxes[0].x === this.food[0] && this.boxes[0].y === this.food[1]){ //ate food
+            s.food = plotNextFood();
         }
-        else{
-            this.grow();
-            if(this.boxes[0].x === this.food[0] && this.boxes[0].y === this.food[1]){ //ate food
-                s.food = plotNextFood();
-            }
-            else{
-                this.remove();
-            }
-        }
-        
+        else if(alive){
+            this.remove();
+        } 
     }
 }
 
@@ -161,7 +186,7 @@ function plotFood(){
     var y = Math.floor(Math.random() * 20) * 25;    //between 0 - 475
     var box = document.getElementById('box-' + x + '-' + y);
     var food = document.createElement('i');
-    food.className = 'fa  fa-crosshairs food';
+    food.className = 'fa  fa-user-circle food';
     food.id = 'food-' + x + '-' + y;
     // food.style.left = x + 'px';
     // food.style.top = y + 'px';
@@ -170,6 +195,8 @@ function plotFood(){
     return [x,y];
 }
 
+
+
 function plotNextFood(){
     var food = s.food;
     var foodNode = document.getElementById('food-' + food[0] + '-' + food[1]);
@@ -177,15 +204,21 @@ function plotNextFood(){
     return plotFood()
 }
 
-var s = new Snake();
 
 async function gameplay(){
     s.food = plotFood();
     while(alive){
         await sleep(speed);
+        if(!alive) break;
         s.move();
         key_allowed = true;
-    }  
+    }
 }
 
-gameplay();
+function startGame(){
+    alive = true;
+    s = new Snake();
+    gameplay();
+}
+
+startGame();
